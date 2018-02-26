136eca646010ed7fcb78940e77c4da1dc26bebfc
Yuriy Kolodiy <macroch@yandex.ru>
2018-02-26 17:44:30 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--closed')) {
+        navMain.classList.remove('main-nav--closed');
+        navMain.classList.add('main-nav--opened');
+    } else {
+        navMain.classList.add('main-nav--closed');
+        navMain.classList.remove('main-nav--opened');
+    }
+});
+
+// let spans = document.querySelectorAll('.site-list__item span');
+document.body.addEventListener('click', function (e) {
+    console.log(e.target);
+    if(e.target.tagName == 'SPAN' ){
+        if(e.target.nextElementSibling){
+            e.target.nextElementSibling.style.display = 'block';
+        }
+    }
+});
