2e16f492fc799476c11f62605a87783128fcd11b
Yuriy Kolodiy <macroch@yandex.ru>
2018-02-28 17:12:30 +0200

    add products block

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ navToggle.addEventListener('click', function() {
 
 // let spans = document.querySelectorAll('.site-list__item span');
 document.body.addEventListener('click', function (e) {
-    console.log(e.target);
     if(e.target.tagName == 'SPAN' ){
         if(e.target.nextElementSibling){
             e.target.nextElementSibling.style.display = 'block';
@@ -52,4 +51,47 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = 'flex';
     dots[slideIndex - 1].className += ' active';
+}
+
+// const slider = document.querySelector('.slider');
+//
+// function mouseOv(e) {
+//     if(e.target.classList.contains('slider')){
+//         window.addEventListener('scroll', function (e) {
+//             plusSlides(1);
+//             return;
+//         });
+//     }
+// }
+//
+// window.addEventListener('mouseover', mouseOv);
+
+let productsDescript = document.querySelectorAll('.item__description p');
+
+for(let i = 0; i < productsDescript.length; i++){
+    let some = [];
+    some[i] = productsDescript[i].innerHTML.split('');
+    some[i].length = 98;
+    productsDescript[i].innerHTML = some[i].join('');
+}
+
+const range = document.querySelector('input[type="range"]');
+const productsItems = document.querySelector('.products__items');
+
+range.addEventListener('input', function () {
+    productsItems.style.marginLeft = -this.value + 'px';
+    console.log(this.value);
+});
+
+if(document.documentElement.clientWidth > 400){
+    range.max = 500;
+}
+if(document.documentElement.clientWidth > 500){
+    range.max = 400;
+}
+if(document.documentElement.clientWidth > 600){
+    range.max = 300;
+}
+if(document.documentElement.clientWidth > 700){
+    range.max = 200;
 }
